a57b2e80a797a165108c6fb2c3691165078dfe41
Torben Giesselmann <git@foogoo.net>
2012-09-21 18:49:02 -0700

    applied cookie changes ... pt. 2

--- src/app/resources/scripts/app.groovy
+++ src/app/resources/scripts/app.groovy
@@ -1,3 +1,15 @@
+delegate 'net.foogoo.ratpack.MyDelegateWithCookies'
+
 get('/') {
    render('index.html')
+}
+
+get('/person/:name') {
+
+    addCookie {
+        name "last-person-viewed"
+        value urlparams.name
+    }
+      
+    render('person.html', [name : urlparams.name])
 }
